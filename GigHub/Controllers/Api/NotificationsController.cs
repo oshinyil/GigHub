312652caba61273f5e7b8b6b20053446fcb44b22
oshinyil <taneyoroshi@gmail.com>
2016-09-11 10:15:27 +0800

    Used AutoMapping for DTO mapping

--- GigHub/Controllers/Api/NotificationsController.cs
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -1,11 +1,9 @@
-﻿using GigHub.Models;
+﻿using AutoMapper;
+using GigHub.Models;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace GigHub.Controllers.Api
@@ -29,25 +27,15 @@ namespace GigHub.Controllers.Api
                     .Include(n => n.Gig.Artist)
                     .ToList();
 
-            return notifications.Select(n => new NotificationDto
+            Mapper.Initialize(cfg =>
             {
-                DateTime = n.DateTime,
-                Gig = new GigDto
-                {
-                    Artist = new UserDto
-                    {
-                        Id = n.Gig.ArtistId,
-                        Name = n.Gig.Artist.Name
-                    },
-                    DateTime = n.Gig.DateTime,
-                    Id = n.Gig.Id,
-                    IsCanceled = n.Gig.IsCanceled,
-                    Venue = n.Gig.Venue
-                },
-                OriginalDateTime = n.OriginalDateTime,
-                OriginalVenue = n.OriginalVenue,
-                Type = n.Type
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Genre, GenreDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
             });
+
+            return notifications.Select(Mapper.Map<Notification, NotificationDto>);
         }
     }
 }
