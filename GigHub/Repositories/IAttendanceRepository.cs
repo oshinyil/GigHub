a0b1f81c11be953f1d9aafaea35fc5e64cfd6438
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-01 13:55:39 +0800

    Extracted interfaces.

---
+++ GigHub/Repositories/IAttendanceRepository.cs
@@ -0,0 +1,12 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.Repositories
+{
+    public interface IAttendanceRepository
+    {
+        IEnumerable<Attendance> GetFutureAttendances(string userId);
+
+        Attendance GetAttendance(int gigId, string userId);
+    }
+}
