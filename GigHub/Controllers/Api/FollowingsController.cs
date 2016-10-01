cb0593c100f99f5b438f2c1985dd1c679a1313e7
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-01 11:05:19 +0800

    Implemented following method on gig detail view.

--- GigHub/Controllers/Api/FollowingsController.cs
+++ GigHub/Controllers/Api/FollowingsController.cs
@@ -21,7 +21,7 @@ namespace GigHub.Controllers.Api
         {
             var userId = User.Identity.GetUserId();
 
-            if (_context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId))
+            if (_context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
             {
                 return BadRequest("Following already exists.");
             }
@@ -35,6 +35,25 @@ namespace GigHub.Controllers.Api
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Unfollow(string id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var following = _context.Followings
+                .SingleOrDefault(f => f.FollowerId == userId && f.FolloweeId == id);
+
+            if (following == null)
+            {
+                return NotFound();
+            }
+
+            _context.Followings.Remove(following);
+            _context.SaveChanges();
+
+            return Ok(id);
         }
     }
 }
---
+++ GigHub/Scripts/app/controllers/gigDetailController.js
@@ -0,0 +1,31 @@
+﻿var GigDetailController = function (followingService) {
+    var followButton;
+
+    var init = function () {
+        $(".js-toggle-follow").click(toggleFollowing);
+    };
+
+    var toggleFollowing = function (e) {
+        followButton = $(e.target);
+        var followeeId = followButton.attr("data-user-id");
+
+        if (followButton.hasClass("btn-default")) {
+            followingService.createFollowing(followeeId, done, fail);
+        } else {
+            followingService.deleteFollowing(followeeId, done, fail);
+        }
+    };
+
+    var fail = function () {
+        alert("Something failed!");
+    };
+
+    var done = function () {
+        var text = followButton.text() === "Follow" ? "Following" : "Follow";
+        followButton.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    return {
+        init: init
+    }
+}(FollowingService);
