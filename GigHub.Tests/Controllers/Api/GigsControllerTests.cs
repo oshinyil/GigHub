81380e1d9b867fa2afb86f0f32b0307c4a2d65c6
Tane Yoroshi <taneyoroshi@gmail.com>
2016-10-02 22:54:18 +0800

    Created gigs unit test.

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using GigHub.Controllers.Api;
+using GigHub.Core;
+using GigHub.Repositories;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Http.Results;
+
+namespace GigHub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+
+        public GigsControllerTests()
+        {
+            var mockRepository = new Mock<IGigRepository>();
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(u => u.Gigs).Returns(mockRepository.Object);
+
+            _controller = new GigsController(mockUoW.Object);
+            _controller.MockCurrentUser("1", "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = _controller.Cancel(1);
+
+            result.Should().BeOfType<NotFoundResult>();
+        }
+    }
+}
